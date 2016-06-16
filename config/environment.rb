015cf3db2f52abca247941ad9a45348d88f614b7
IshmaelKhalid <ishmaelkhalid@icloud.com>
2016-06-16 12:04:31 -0400

    Env Update
    Updated environment.rb to link to version file in working directory.

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+#This page was setup to make code easier to read and access, using standard folder structure (config) and descriptive file name (environment)
+
+#require 'pry' # for testing
+#require 'nokogiri' # for scraping kickstarter website
+#require 'open-uri' # for scraping kickstarter website
+
+# require all files in lib directory
+require_relative '../lib/NewKS/version'
+#require_relative '../lib/NewKS/project'
+#require_relative '../lib/NewKS/cli'
