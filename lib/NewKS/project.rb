0a3df5444d214b4068da86dddc761c565dcfb600
IshmaelKhalid <ishmaelkhalid@icloud.com>
2016-06-16 13:14:40 -0400

    all and find methods
    added all and find methods to project.rb

--- lib/NewKS/project.rb
+++ lib/NewKS/project.rb
@@ -1,5 +1,15 @@
 class NewKS::Project
   attr_accessor :name, :url, :summary, :author
   # reader and writer methods for my project properties
-  
+
+  def self.all
+    # fetch all projects if available or scrape KS
+    @@all ||= scrape_newest_projects
+  end
+
+  def self.find(id)
+    # find method that searches all projects by index
+    # [id-1] for offset, id starts @ 1, index starts @ 0
+    self.all[id-1]
+  end
 end
