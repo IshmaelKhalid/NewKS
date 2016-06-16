796d6d7cf7416e9ed98fa301095dc80cf28b6fa1
IshmaelKhalid <ishmaelkhalid@icloud.com>
2016-06-16 13:08:07 -0400

    list method
    corrected class error, and added list method to cli.rb

--- lib/NewKS/cli.rb
+++ lib/NewKS/cli.rb
@@ -1,9 +1,21 @@
-class NewestKickstarter::CLI
+class NewKS::CLI
   
   def start # Dictates the flow and order of my gem  
     list # Call Scraping Method
     menu # Call Interactive Method
     goodbye # Call Closing Method
   end
-  
+
+  def list # Scraping Method
+    puts "" #Skips Line
+    puts "************* (Top 20) Newest Kickstarter Projects *************" # List Title
+    puts "" #Skips Line
+    #Call .all method on Project Class, take each with index and puts
+    NewKS::Project.all.each.with_index(1) do |project, i|
+      puts "#{i}. #{project.name}" 
+      #format: 1. blahblah (then skips line, for each project)
+    end
+    puts "" #Skips Line
+  end
+
 end
