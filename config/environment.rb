75803229b3da41567f63347f21cd3a4d0489f3c3
IshmaelKhalid <ishmaelkhalid@icloud.com>
2016-06-16 13:34:41 -0400

    bug fixes
    rebuilt gem under same version, edited gem file and gemspec to remove
    ‘pry’ as it caused issues in deployment, removed pry from environment
    as well.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 #This page was setup to make code easier to read and access, using standard folder structure (config) and descriptive file name (environment)
 
-require 'pry' # for testing
 require 'nokogiri' # for scraping kickstarter website
 require 'open-uri' # for scraping kickstarter website
 
